661be2a5d35110021803e0466c3ffffbd2365183
Bobby Allen <ballen@bobbyallen.me>
2015-12-31 19:10:13 +0000

    Checking of required PHP extentions and added a couple of new configuration options.

--- hooker.php
+++ hooker.php
@@ -9,18 +9,49 @@
  *
  */
 $config = [
+
+    /**
+     * The remote repository to pull/checkout form.
+     * Example: git@github.com:bobsta63/hooker.git
+     */
+    'repo' => '',
     
+    /**
+     * Which branch to pull/checkout from.
+     * Example: master
+     */
+    'branch' => 'master',
     /**
      * Enable debug mode - Will output errors to the screen.
      */
     'debug' => false,
+    /**
+     * Pre-deploy commands to run.
+     */
+    'post_commands' => [
+    ],
+    /**
+     * Post-deploy commands to run.
+     */
+    'post_commands' => [
+    ],
 ];
 
 /**
  * End or user configuration - It is not recommended that you edit below this line!
  */
+if (isset($_REQUEST['ping'])) {
+    echo 'PONG';
+    exit;
+}
 
 if (file_exists('hooker.conf.php')) {
     $config_file = require_once 'hooker.conf.php';
     $config = array_merge($config, $config_file);
 }
+
+if((!function_exists('shell_exedc')) && $config['debug']){
+    echo 'The PHP function shell_exec() does not exist!';
+    exit;
+}
+
