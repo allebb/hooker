3ed9b14625354737d2520c51f693531557b6eca4
Bobby Allen <ballen@bobbyallen.me>
2026-07-16 21:56:11 +0100

    default tweak.

--- webadmin/auth.php
+++ webadmin/auth.php
@@ -3,10 +3,10 @@
 return [
 
     // Whether the webadmin feature is enabled. Set this to false to disable the webadmin feature entirely (and return a 404 for all webadmin requests).
-    'enable_webadmin_feature' => true,
+    'enable_webadmin_feature' => false,
 
     // The username and password for the webadmin feature. Change these to secure your installation.
     'username' => 'admin',
     'password' => 'change-me',
-    
+
 ];
