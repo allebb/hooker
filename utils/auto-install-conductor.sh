c1492cb56678f8b75a16ff800176da9644ac2156
Bobby Allen <ballen@bobbyallen.me>
2026-07-17 20:44:05 +0100

    Further integration improvements for Hooker.

--- utils/auto-install-conductor.sh
+++ utils/auto-install-conductor.sh
@@ -5,10 +5,11 @@
 #
 #
 
+set -e
+
 # Check the Conductor version/if it's installed
 echo "Checking Conductor is installed..."
-conductor --version > /dev/null
-if [ $? -eq 0 ]; then
+if conductor --version > /dev/null; then
     echo " * Conductor has been found, continuing..."
 else
     echo " ! Conductor does not appear to be installed, exiting!"
@@ -22,39 +23,38 @@ read -p 'What directory should we deploy Hooker to? (eg: "hooker"): ' appname
 read -p 'What domain do you want Hooker to be hosted on? (eg: "deploy.mysite.com"): ' fqdn
 
 # Call the CLI command (using sudo) to create the hosting environment.
-sudo conductor new $appname --fqdn="${fqdn}" --path="/" > /dev/null
-if [ $? -ne 0 ]; then
+if ! sudo conductor new "$appname" --fqdn="${fqdn}" --path="/" > /dev/null; then
     echo " ! Could not create an application hosting container using Conductor, exiting!"
     exit 1
 fi
 
 # Git clone (as www-data user) to the /var/conductor/application/{name} directory
 echo "Installing Hooker..."
-cd /var/conductor/applications/$appname
-sudo -u www-data git clone https://github.com/allebb/hooker.git .
-sudo -u www-data git checkout stable
+appdir="/var/conductor/applications/${appname}"
+sudo find "$appdir" -mindepth 1 -maxdepth 1 -exec rm -rf {} +
+cd "$appdir"
+sudo -u www-data git clone --branch stable --single-branch https://github.com/allebb/hooker.git .
 
 # Configure a cache directory for Composer (Latest version of Conductor now handles this at installation).
 #sudo mkdir /var/www/.cache
 #sudo chown -R www-data:www-data /var/www/.cache
 
 # Copy the hooker.conf.example file to hooker.conf.php
-sudo -u www-data cp /var/conductor/applications/$appname/hooker.conf.example-clean.php /var/conductor/applications/$appname/hooker.conf.php
+sudo -u www-data cp "${appdir}/hooker.conf.example-clean.php" "${appdir}/hooker.conf.php"
 
 # Install the Hooker command line helper
-sudo cp /var/conductor/applications/$appname/utils/hooker /usr/bin/hooker
+sudo cp "${appdir}/utils/hooker" /usr/bin/hooker
 sudo chmod +x /usr/bin/hooker
 
 # Copy the default Nginx virtualhost configuration to the Conductor application configuration...
-sudo cp /var/conductor/applications/$appname/utils/auto-install-conductor_nginx.conf /etc/conductor/configs/$appname.conf
+sudo cp "${appdir}/utils/auto-install-conductor_nginx.conf" "/etc/conductor/configs/${appname}.conf"
 
 # Update the FQDN in the virtualhost configuration file
-sudo sed -i "s/__FQDN__/${fqdn}/g" /etc/conductor/configs/$appname.conf
-sudo sed -i "s/__APPNAME__/${appname}/g" /etc/conductor/configs/$appname.conf
+sudo sed -i "s/__FQDN__/${fqdn}/g" "/etc/conductor/configs/${appname}.conf"
+sudo sed -i "s/__APPNAME__/${appname}/g" "/etc/conductor/configs/${appname}.conf"
 
 # Test that the Nginx configuration passes validation...
-sudo nginx -t > /dev/null
-if [ $? -eq 0 ]; then
+if sudo nginx -t > /dev/null; then
     echo " * Nginx configuration passes validation..."
 else
     echo " ! Something went wrong with the Nginx configuration validation, check your nginx configurations!"
@@ -66,10 +66,11 @@ sudo service nginx restart
 echo ""
 
 echo "Now creating an SSH key (which should be used for automated Git functionality)..."
-sudo mkdir /var/www/.ssh
+sudo mkdir -p /var/www/.ssh
 sudo chown www-data:www-data -R /var/www/.ssh
-sudo -u www-data ssh-keygen -t rsa -b 4096 -N "" -C "hooker@${fqdn}" -q -f /var/www/.ssh/id_rsa > /dev/null
-if [ $? -ne 0 ]; then
+if [ -f /var/www/.ssh/id_rsa ]; then
+    echo " * An SSH key already exists at /var/www/.ssh/id_rsa, leaving it unchanged."
+elif ! sudo -u www-data ssh-keygen -t rsa -b 4096 -N "" -C "hooker@${fqdn}" -q -f /var/www/.ssh/id_rsa > /dev/null; then
     echo " ! Could not create an SSH key, please create one manually for the www-data user!"
 else
 	echo ""
@@ -83,6 +84,42 @@ else
 	echo ""
 fi
 sudo chmod -R 0700 /var/www/.ssh
+
+echo ""
+read -r -p 'Do you want to enable the Web Admin panel? [y/N]: ' enable_webadmin
+case "$enable_webadmin" in
+    [Yy]|[Yy][Ee][Ss])
+        while [ -z "${webadmin_username:-}" ]; do
+            read -r -p 'Web Admin username: ' webadmin_username
+        done
+
+        while [ -z "${webadmin_password:-}" ]; do
+            read -r -s -p 'Web Admin password: ' webadmin_password
+            echo ""
+        done
+
+        sudo -u www-data php -r '
+            $path = $argv[1];
+            $username = var_export($argv[2], true);
+            $password = var_export($argv[3], true);
+            $source = "<?php\n\nreturn [\n\n";
+            $source .= "    // Whether the webadmin feature is enabled. Set this to false to disable the webadmin feature entirely (and return a 404 for all webadmin requests).\n";
+            $source .= "    '\''enable_webadmin_feature'\'' => true,\n\n";
+            $source .= "    // The username and password for the webadmin feature. Change these to secure your installation.\n";
+            $source .= "    '\''username'\'' => {$username},\n";
+            $source .= "    '\''password'\'' => {$password},\n\n";
+            $source .= "];\n";
+            if (file_put_contents($path, $source) === false) {
+                fwrite(STDERR, "Unable to update {$path}\n");
+                exit(1);
+            }
+        ' "${appdir}/webadmin/auth.php" "$webadmin_username" "$webadmin_password"
+        echo " * Web Admin panel enabled."
+        ;;
+    *)
+        echo " * Web Admin panel left disabled."
+        ;;
+esac
 
 echo "You can now configure your workflows/deployments by updating the global Hooker configuration:"
 echo ""
